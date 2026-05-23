b2cd0f0b909a723eb0ce1b51ef284b3aafc536f2
pamechavz <clopezpamelanah@uss.edu.pe>
2026-05-23 15:55:04 -0500

    test: [TDD - RED] Pruebas unificadas 10 y 11 para validacion de recargas

--- apps/compliance/tests/test_deposit_limits.py
+++ apps/compliance/tests/test_deposit_limits.py
@@ -3,10 +3,12 @@ from decimal import Decimal
 from django.test import TestCase
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError 
 from apps.compliance.services import (
     get_daily_limit, 
     set_daily_limit, 
-    promote_pending_limits
+    promote_pending_limits,
+    validate_deposit  
 )
 
 User = get_user_model()
@@ -27,16 +29,23 @@ class DepositLimitTests(TestCase):
         set_daily_limit(self.user, Decimal('2000.0000'))
         limit_data = get_daily_limit(self.user)
         self.assertEqual(limit_data['active_limit'], Decimal('500.0000')) 
-        # Los 2000 se quedan en cola de espera
         self.assertEqual(limit_data['pending_limit'], Decimal('2000.0000'))
-        
+
         limit_obj = self.user.depositlimit 
         limit_obj.pending_since = timezone.now() - datetime.timedelta(hours=25)
         limit_obj.save()
 
-
         promote_pending_limits() 
-
+        
         limit_data = get_daily_limit(self.user)
         self.assertEqual(limit_data['active_limit'], Decimal('2000.0000'))
         self.assertIsNone(limit_data['pending_limit'])
+
+    def test_10_11_validacion_recargas_segun_limite(self):
+        """10 y 11: Recarga dentro del límite se permite, si lo excede se rechaza RED → GREEN"""
+        set_daily_limit(self.user, Decimal('500.0000'))
+
+        self.assertTrue(validate_deposit(self.user, Decimal('200.0000')))
+
+        with self.assertRaisesMessage(ValidationError, "El monto de recarga excede el límite diario configurado"):
+            validate_deposit(self.user, Decimal('600.0000'))
