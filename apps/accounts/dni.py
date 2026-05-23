02bbc8cb91c80c120e2e91ab431882423e03d86a
Maicol Rafael <maicolnelsonrafaelrojas@gmail.com>
2026-05-23 14:42:25 -0500

    feat: [TDD - GREEN] implementar algoritmo modulo 11 para digito verificador

--- apps/accounts/dni.py
+++ apps/accounts/dni.py
@@ -1,2 +1,10 @@
 def calcular_digito_verificador(dni_str: str) -> str:
-    return ""
+    pesos = [3, 2, 7, 6, 5, 4, 3, 2]
+    suma = sum(int(dni_str[i]) * pesos[i] for i in range(8))
+    residuo = suma % 11
+    
+    tabla_mapeo = {
+        0: "K", 1: "0", 2: "1", 3: "2", 4: "3", 5: "4",
+        6: "5", 7: "6", 8: "7", 9: "8", 10: "9"
+    }
+    return tabla_mapeo[residuo]
